e838419bd575fc1f8395e49d25d56e476a893f20
GlitchGirl0404 <101594414+GlitchGirl0404@users.noreply.github.com>
2025-02-24 15:43:19 +0100

    Fixed the level select bug

---
+++ Assets/GameManager.cs
@@ -0,0 +1,36 @@
+using TMPro;
+using UnityEngine;
+public class GameManager : MonoBehaviour
+{
+    public class Nivo
+    {
+        public string flight_code;
+        public string city;
+        public string airport;
+        public Nivo(string flightCode, string city, string airport)
+        {
+            this.flight_code = flightCode;
+            this.city = city;
+            this.airport = airport;
+        }
+    }
+    public Nivo[] nivoi = new Nivo[4];
+    [SerializeField] TextMeshProUGUI flight_code;
+    [SerializeField] TextMeshProUGUI airport_name;
+    [SerializeField] TextMeshProUGUI city;
+    public int current = 0;
+    public void DisplayNames()
+    {
+        flight_code.text = nivoi[current].flight_code;
+        city.text = nivoi[current].city;
+        airport_name.text = nivoi[current].airport;
+    }
+    void Start()
+    {
+        nivoi[0] = new Nivo("V3CUHY", "Belgrade", "Nikola Tesla");
+        nivoi[1] = new Nivo("QL6PLM", "Murcia", "Región de Murcia International Airport");
+        nivoi[2] = new Nivo("2P6GYI", "Basel", "EuroAirport Basel Mulhouse Freiburg");
+        nivoi[3] = new Nivo("M478AT", "Free Play", "######");
+        DisplayNames();
+    }
+}
--- Assets/Scripts/LevelSelectButtons.cs
+++ Assets/Scripts/LevelSelectButtons.cs
@@ -1,39 +1,31 @@
 using TMPro;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UIElements;
+using System;
 public class LevelSelectButtons : MonoBehaviour
 {
-    public class Nivo
+    GameManager game_manager;
+    [SerializeField] GameObject gm;
+    void Awake()
     {
-        public string flight_code;
-        public string city;
-        public string airport;
-        public int level;
-        public Nivo(string flightCode, string city, string airport, int level)
-        {
-            this.flight_code = flightCode;
-            this.city = city;
-            this.airport = airport;
-            this.level = level;
-        }
-    }
-    public Nivo[] nivoi = new Nivo[4];
-    public TextMeshPro flight_code;
-    public TextMeshPro airport_name;
-    public TextMeshPro City;
-    void Start()
-    {
-        nivoi[0] = new Nivo("V3CUHY", "Belgrade", "Nikola Tesla", 1);
-        nivoi[1] = new Nivo("QL6PLM", "Murcia", "Región de Murcia International Airport", 2);
-        nivoi[2] = new Nivo("2P6GYI", "Basel", "EuroAirport Basel Mulhouse Freiburg", 3);
-        nivoi[3] = new Nivo("M478AT", "Free Play", "######", 4);
+        game_manager = gm.GetComponent<GameManager>();
     }
     public void Left()
     {
-
+        if (game_manager.current > 0)
+        {
+            game_manager.current = game_manager.current - 1;
+        }
+        game_manager.DisplayNames();
     }
     public void Right()
     {
-
+        if (game_manager.current < game_manager.nivoi.Length - 1)
+        {
+            game_manager.current = game_manager.current + 1;
+        }
+        game_manager.DisplayNames();
     }
     public void Play()
     {
