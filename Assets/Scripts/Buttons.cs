b6100cdd6bddfda1a2d12b204246b45e0f3fdd75
GlitchGirl0404 <101594414+GlitchGirl0404@users.noreply.github.com>
2025-04-27 17:38:07 +0200

    Fixed the bug where maturski wouldn't open

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Buttons : MonoBehaviour
@@ -8,7 +9,7 @@ public class Buttons : MonoBehaviour
     }
     public void Settings()
     {
-        Application.OpenURL("file:///" + Application.dataPath + "\\Maturski.pdf");
+        Application.OpenURL("https://docs.google.com/document/d/1k-_Bal8zLgYoMlPhAfHNqhmanNvaf3yUaKWKN2v8-dE/edit?usp=sharing");
     }
     public void Exit()
     {
